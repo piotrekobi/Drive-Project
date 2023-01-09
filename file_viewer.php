0df8f66cd78ff2eff504fca3b956925c46b6e584
Kacper Maj <k.maj@stud.elka.pw.edu.pl>
2023-01-09 20:10:31 +0100

    Replace file_viewer.php

--- file_viewer.php
+++ file_viewer.php
@@ -62,6 +62,12 @@ include "funcs.php";
     $preview = "<img class='card-img-top' alt='' src='$path'>";
     $file_size = human_filesize(filesize($path));
     $date_modified = date("M j Y", filemtime($path));
+
+    $pieces = explode("/", $file);
+    $sliced = array_slice($pieces, 0, -1);
+    $temp = end($pieces);
+    $string = implode("/", $sliced);
+
     if (is_dir($path)) {
         $path = get_url_path($path);
         $onclick_path = "index.php?location=$path";
@@ -71,7 +77,7 @@ include "funcs.php";
     $html_code .= "<div class='card file-card m-2' style='width: 18rem;'>
                             $preview    
                             <div class='card-body'> 
-                                <h5 class='card-title'><a href='#' class='stretched-link'>$file</a></h5>
+                                <h5 class='card-title'><a href='#' class='stretched-link'>$temp</a></h5>
                                 <div class='d-flex justify-content-between align-items-center'>
                                     <p class='card-text mb-0'><small class='text-muted'>$date_modified</small></p>
                                     <p class='card-text'><small class='text-muted'>$file_size</small></p>
@@ -79,13 +85,13 @@ include "funcs.php";
                             </div>
                         </div>
                         <div>
-                            <div class='form-floating'>
-                                <input type='text' class='form-control' id='new_name' name='new_name'>
-                                <label for='username'>Nowa nazwa</label>
-                            </div>
                             <form method='post'>
-                                <button class='renameButton' type='submit' value='renButton'/>zmień nazwę</button>
-                                <button class='deleteButton' type='submit' value='delButton'/>usuń</button>
+                                <div>
+                                    <input type='text' class='form-control' id='new_name' name='new_name'>
+                                    <label for='username'>Nowa nazwa</label>
+                                </div>
+                                <button class='renameButton' type='submit' name='renButton' value='renButton'/>zmień nazwę</button>
+                                <button class='deleteButton' type='submit' name='delButton' value='delButton'/>usuń</button>
                             </form>
                             <div>
                                 <button class='retButton' onclick=\"location.href='/../index.php?location=Home';\">Powrót</button>
@@ -98,12 +104,14 @@ include "funcs.php";
     ?>
 </center>
 <?php
-    if(isset($_POST['renButton']) && !empty($_POST['new_name'])) {
+    if(array_key_exists('renButton', $_POST) && !empty($_POST['new_name'])) {
         $new_name = $_POST['new_name'];
-        rename($file, $string + '/' + $new_name);
+        rename($file, $string.'/'.$new_name);
+        header('Location: /../index.php?location=Home');
+        exit();
     }
-    if(isset($_POST['delButton'])) {
-        rmdir('/../'.$file);
+    else if(array_key_exists('delButton', $_POST)) {
+        unlink($_SERVER['DOCUMENT_ROOT'].'/'.$file);
         header('Location: /../index.php?location=Home');
         exit();
     }
