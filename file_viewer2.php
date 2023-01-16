fc9831a925eed6601cfcd6cf8c79480cbd31d42c
Kacper Maj <k.maj@stud.elka.pw.edu.pl>
2023-01-16 01:30:35 +0100

    Replace file_viewer2.php

--- file_viewer2.php
+++ file_viewer2.php
@@ -5,24 +5,63 @@ include "funcs.php";
 <html>
 
 <head>
+    <link rel="stylesheet" href="button_styles.css">
     <?php include "shared/head.php"; ?>
     <title>Podgląd</title>
 </head>
 <?php include "shared/protect_files.php"; ?>
 <body>
-    <div class="container-fluid px-1 vh-100">
-        <?php include "shared/header_bar.php"; ?>
-        <main class="h-75 align-content-center pt-3">
-            <!-- Page content -->
-            Hello world
-            <button type="button" class="btn btn-primary">Primary</button>
-            <?php print($_GET['file']);
-            $TTT = $_GET['file'];
-            $LOL = "<img class='card-img-top' alt='' src='$TTT'>";
-            print($LOL);
-            ?>
-        </main>
-    </div>
+    <center>
+        <div class="container-fluid px-1 vh-100">
+            <?php include "shared/header_bar.php"; ?>
+            <main class="h-75 align-content-center pt-3">
+                <!-- Page content -->
+                Podgląd pliku
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <?php 
+                        print($_GET['file']);
+                        $TTT = $_GET['file'];
+                        $LOL = "<img class='card-img-top' alt='' src='$TTT'>";
+                        print($LOL);?>
+                    </div>
+                </div>
+                
+                <div>
+                    <div>
+                        <div class="card" style="width: 18rem;">
+                            <input type='text' class='form-control' id='new_name' name='new_name'>
+                            <label for='username'>Nowa nazwa</label>
+                        </div>
+                    </div>
+                    <div>
+                        <button type="submit" class="renameButton" name='renButton' value='renButton'>zmień nazwę</button>
+                        <button type="submit" class="deleteButton" name='delButton' value='delButton'>usuń</button>
+                    </div>
+                    <button type="submit" class="retButton" onclick="location.href='/../index.php?location=Home';">Powrót</button>
+                </div>
+
+                <?php if(array_key_exists('renButton', $_POST) && !empty($_POST['new_name'])) {
+                    $new_name = $_POST['new_name'];
+                    rename($_GET['file'], $new_name);
+                    ?>
+                    <script type="text/javascript">
+                    window.location = "/../index.php?location=Home";
+                    </script>
+                    <?php
+                }
+                else if(array_key_exists('delButton', $_POST)) {
+                    unlink($_SERVER['DOCUMENT_ROOT'].'/'.$_GET['file']);
+                    ?>
+                    <script type="text/javascript">
+                    window.location = "/../index.php?location=Home";
+                    </script>
+                    <?php
+                }
+                ?>
+            </main>
+        </div>
+    </center>
 </body>
 
 </html>
